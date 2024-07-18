17e622212a8eca24f8ca129395a50a06d56ec7ac
drtun <drtun@live.com>
2024-07-19 01:25:25 +0630

    SQFlite added

--- lib/src/views/view_data_list.dart
+++ lib/src/views/view_data_list.dart
@@ -1,39 +1,96 @@
-import 'package:flutter/material.dart';  
+
+import 'package:flutter/material.dart';
 import 'view_data_details.dart';
-//  -------------------------------------    Databases (Property of Nirvasoft.com)
+import '../sqflite/sqf_helpers.dart';
+import '../sqflite/sqf_models.dart';
+
 class ViewDataList extends StatefulWidget {
-  static const routeName = '/formdatalist';
   const ViewDataList({super.key});
+  static const routeName = '/NoteList';  
+
   @override
-  State<ViewDataList> createState() => _ViewDataListState();
+  State<ViewDataList> createState() => _FormDataListState();
 }
-class _ViewDataListState extends State<ViewDataList> { 
-  final dataController = TextEditingController(); 
-  @override
-  void initState() {
-    super.initState();   
+
+class _FormDataListState extends State<ViewDataList> {
+
+  late final ThemeData _theme = Theme.of(context);
+  late final ColorScheme _colors = _theme.colorScheme;
+  late final TextTheme _textTheme = _theme.textTheme;
+
+  Widget noteCard(NoteModel? note){
+    return InkWell(
+      onTap:()async{
+        await Navigator.pushNamed(context, ViewDataDetails.routeName,arguments: note);
+        setState(() {});
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3.0,horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                note?.title??"",
+                style :_textTheme.bodyLarge!.copyWith(color: _colors.onSurface)
+              ),
+              Text(
+                note?.description??"",
+                style :_textTheme.bodyMedium!.copyWith(color: _colors.onSurfaceVariant)
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //-----------------------------appbar
       appBar: AppBar(
-        title: const Text('List'),
-      ), 
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 10), 
-          const Text("Show the list"),
-          const SizedBox(height: 10), 
-          ElevatedButton(
-            onPressed: () async {
-              Navigator.pushNamed(context,ViewDataDetails.routeName, );  
-            },
-            child: const Text('Go to Details'),
-          ),
-        ],
-      )
+        title: const Text('NoteList'),
+      ),
+
+      //-----------------------------NoteList
+      body: FutureBuilder<List<NoteModel>>(
+        future: DatabaseHelper().getAllNoteData(), 
+        builder: (BuildContext context,AsyncSnapshot snapshot){
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return noteCard(snapshot.data[index]);
+              },
+            );
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      ),
+
+
+      //----------------------------floatingActionButton
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()async{
+          final result = await Navigator.pushNamed(
+            context,
+            ViewDataDetails.routeName
+          );
+          if(result!=null){
+            //widget rebuild for getNoteList again
+            setState(() {});
+          }
+
+          
+        },
+        tooltip: "Add Note",
+        child: const Icon(Icons.add),
+      ),
     );
-  }
-  Future<void> doIt() async { 
   }
 }

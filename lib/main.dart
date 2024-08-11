c1bf92b5e6394cd29e926829e806ea2a777aaf43
drtun <drtun@live.com>
2024-08-11 20:38:05 +0630

    splash added

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,13 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 //  Version 1.0.2
 //  -------------------------------------    Main (Property of Nirvasoft.com) 
 void main() async {   
   // (A) Native Splash Screen if needed
-  //WidgetsBinding widgetsBinding = 
-  WidgetsFlutterBinding.ensureInitialized();
-  //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   //​ (B) Preloading  
   // 1) Settings  
   final settingsController = SettingsController(SettingsService()); 
@@ -68,7 +67,7 @@ void main() async {
     FirebaseMessaging.instance.getToken().then((value) =>   showToken(value));
   }  
   // (E) Remove Splash Screen
-    //FlutterNativeSplash.remove();
+  FlutterNativeSplash.remove();
 } 
 
 // --------------------------------------------------- END of main() ------------------
